6e5ceab5cead7d40dff087a60462201f9c6baf82
hamzeasadi <hamzeasadi1@gmail.com>
2023-01-27 12:48:04 +0000

    local working

--- src/engine.py
+++ src/engine.py
@@ -20,7 +20,7 @@ def train_step(disc: nn.Module, gen: nn.Module, data: DataLoader, criterion: nn.
         X1 = X1.to(dev).squeeze()
         X2 = X2.to(dev).squeeze()
         fake, real = gen(X1, X2)
-        print(fake.shape, real.shape)
+        
         discreal = real.detach().to(dev)
         discfake = fake.detach().to(dev)
         for _ in range(5):
@@ -35,6 +35,7 @@ def train_step(disc: nn.Module, gen: nn.Module, data: DataLoader, criterion: nn.
 
         # gen training
         disc_out = disc(fake).reshape(-1)
+        print(fake.shape, real.shape)
         gen_loss1 = criterion(fake, real)
         gen_loss2 = -torch.mean(disc_out)
         gen_loss = gen_loss1 + gen_loss2
