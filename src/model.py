811168022016bbb8d3b5e46504530d1e17acca44
hamzeasadi <hamzeasadi1@gmail.com>
2023-02-18 23:28:52 +0000

    new reg

--- src/model.py
+++ src/model.py
@@ -55,9 +55,9 @@ class Disclocal(nn.Module):
     def __init__(self, inch) -> None:
         super().__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(in_channels=inch, out_channels=16, kernel_size=4, stride=2, padding=1), nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1), nn.BatchNorm2d(32), nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1), nn.BatchNorm2d(64), nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(in_channels=inch, out_channels=16, kernel_size=4, stride=2, padding=1), nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1), nn.BatchNorm2d(32), nn.LeakyReLU(0.2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=1), nn.BatchNorm2d(64), nn.LeakyReLU(0.2),
             nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1),
         )
 
