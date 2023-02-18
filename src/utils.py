17b79c356d8ca861ecca2df6c5f6e145679df13f
hamzeasadi <hamzeasadi1@gmail.com>
2023-02-18 23:20:25 +0000

    new reg

--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,21 @@ def calc_psd(x):
     r = torch.mean(torch.log(avgpsd)) - torch.log(torch.mean(avgpsd))
     return r
 
+def euclidean_distance_matrix(x):
+    eps = 1e-8
+    x = torch.flatten(x, start_dim=1)
+    # dot_product = torch.mm(x, torch.transpose(x, 1, 0))
+    # xt = x
+    dot_product = torch.mm(x, x.t())
+    squared_norm = torch.diag(dot_product)
+    distance_matrix = squared_norm.unsqueeze(0) - 2 * dot_product + squared_norm.unsqueeze(1)
+    distance_matrix = F.relu(distance_matrix)
+    # distformas = distance_matrix.clone()
+    mask = (distance_matrix == 0.0).float()
+    distance_matrix = distance_matrix.clone() + mask * eps
+    distance_matrix = torch.sqrt(distance_matrix)
+    distance_matrix = distance_matrix.clone()*(1.0 - mask)
+    return distance_matrix
 
 class KeepTrack():
     def __init__(self, path) -> None:
