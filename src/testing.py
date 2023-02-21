6971ece602ef8fe34b34368de5d7f06851ceaf4e
hamzeasadi <hamzeasadi1@gmail.com>
2023-02-21 11:54:55 +0000

    new model

---
+++ src/testing.py
@@ -0,0 +1,68 @@
+import os
+import conf as cfg
+import torch
+import cv2
+from sklearn.metrics import confusion_matrix, auc, roc_auc_score, roc_curve, accuracy_score
+import numpy as np
+import utils
+import model as m
+import dnCNN as dncnn
+from matplotlib import pyplot as plt
+from torch import nn as nn
+import argparse
+
+parser = argparse.ArgumentParser(prog='testing.py')
+parser.add_argument('--mn', type=str)
+
+args = parser.parse_args()
+
+
+
+def main():
+    img = cv2.imread(os.path.join(cfg.paths['data'], 'video1iframe0.bmp'))
+    # img0 = 2*(img[300:700, 850:1250, 1:2] - np.min(img[:, :, 1:2] ))/(np.max(img[:, :, 1:2] ) - np.min(img[:, :, 1:2] ) + 1e-5) -1
+    # img0 = 1*(img[:, :, 1:2] - np.min(img[:, :, 1:2] ))/(np.max(img[:, :, 1:2] ) - np.min(img[:, :, 1:2] ))
+
+    # img0 = (img[100:200, 100:200, 1:2] - 0)/255
+    img0 = (img[240:840, 360:1560, 1:2] - 0)/255
+    img0[100:300, 300:500, :] = img0[300:500, 500:700, :]
+    
+    # img0 = (img[300:700, 850:1250, 1:2] - 127 )/255
+    imgt = torch.from_numpy(img0).permute(2, 0, 1).unsqueeze(dim=0).float()
+
+    # imgt = torch.from_numpy(img0).permute(2, 0, 1).float()
+    # xx = torch.zeros(size=(200, 64, 64))
+    # for i in range(10):
+    #     hi = i*64
+    #     for j in range(20):
+    #         wi = j*64
+    #         xx[i*20+j] = imgt[0, hi:hi+64, wi:wi+64]
+    
+    # r = calc_psd(xx)
+    # print(r)
+
+ 
+    kt = utils.KeepTrack(path=cfg.paths['model'])
+    listofmodels = os.listdir(cfg.paths['model'])
+    # state = kt.load_ckp(fname=listofmodels[-1])
+    # state = kt.load_ckp(fname=f'noisprintcoord2_{50}.pt')
+    state = kt.load_ckp(fname=f'{args.mn}.pt')
+    print(state['trainloss'], state['valloss'])
+    model = dncnn.DnCNN(inch=1, depth=15)
+    # model = nn.DataParallel(model)
+    model.load_state_dict(state['model'], strict=True)
+    model.eval()
+    with torch.no_grad():
+        out = model(imgt)
+        print(out.shape)
+    
+    img1 = out.detach().squeeze().numpy()
+    # imgout = 255*((img1 - np.min(img1))/(np.max(img1) - np.min(img1)))
+
+    plt.imshow(img1, cmap='gray')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
